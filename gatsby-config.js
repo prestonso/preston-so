941b14d515a3f21015d54861be00f09409c60234
Preston So <pso@post.harvard.edu>
2019-04-03 23:57:05 -0400

    Add gatsby-source-drupal and Drupal API config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `preston.so`,
+    description: `Product strategist, innovation lead, developer advocate`,
+    author: `@prestonso`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -30,5 +30,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: `http://preston-so-edit.dd:8083/`,
+      },
+    },
   ],
 }
