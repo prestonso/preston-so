3fca926b3b5f6351e603eb514897f6aaef021bc0
Preston So <pso@post.harvard.edu>
2019-04-03 23:57:51 -0400

    Add images

--- src/components/header.js
+++ src/components/header.js
@@ -3,30 +3,29 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <h1 style={{
+      padding: `2rem 1.5rem`,
+      fontFamily: `'basic-sans', 'Droid Sans', sans-serif`,
+    }}>
+      <Link to="/">
+        {siteTitle}
+      </Link>
+    </h1>
+    <nav className="header__nav">
+      <ul>
+        <li><Link to="/writing/" activeClassName="active">Writing</Link></li>
+        <li><Link to="/speaking/" activeClassName="active">Speaking</Link></li>
+        <li><Link to="/press/" activeClassName="active">Press</Link></li>
+        <li><Link to="/about/" activeClassName="active">About</Link></li>
+        <li><Link to="/contact/" activeClassName="active">Contact</Link></li>
+      </ul>
+    </nav>
+    <div className="header__language-selector">
+      <p><strong>Decoupled Drupal in Practice</strong>: The comprehensive guide to decoupled Drupal across the stack.</p>
+      <ul>
+        <li><a href="#">Available now on Amazon.com</a></li>
+      </ul>
     </div>
   </header>
 )
