24723ae713ede69dd6fd48bd5b5f280d74c9f3bb
Preston So <pso@post.harvard.edu>
2019-06-06 11:55:35 -0400

    Add article component and update writing page

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -5,8 +5,9 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import Card from "../components/card"
+import Article from "../components/article"
 
-const WritingPage = () => (
+const WritingPage = ({ data }) => (
   <Layout>
     <SEO title="Writing" keywords={[`blog`, `articles`, `writing`, `tutorials`]} />
     <Card
@@ -15,7 +16,35 @@ const WritingPage = () => (
       title="Writing"
       body={<p>Coming soon. <Link to="/">Back to preston.so</Link></p>}
     />
+    <Card
+      type="main"
+      orientation="no"
+      title="Articles"
+      body={
+        <>
+          {data.allContentfulArticle.edges.map(({ node }) => (
+            <div>
+              <Article article={node} />
+            </div>
+          ))}
+        </>
+      }
+    />
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allContentfulArticle(sort: { fields: [originalPublicationDate], order: DESC }) {
+      edges {
+        node {
+          title
+          slug
+          originalPublicationDate(formatString: "MMMM Do, YYYY")
+        }
+      }
+    }
+  }
+`
 
 export default WritingPage
