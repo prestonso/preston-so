4f95602109c7bce78abf42af4d5b7087204d0026
Preston So <pso@post.harvard.edu>
2019-07-26 09:59:37 -0400

    Add podcast to list of possible contact reasons

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,7 +13,7 @@ const ContactPage = () => (
       type="intro"
       orientation="no"
       title="Contact"
-      body={<p>Interested in hiring me for a <strong>talk</strong>, <strong>webinar</strong>, <strong>workshop</strong>, or <strong>writing</strong>? Please contact me through the form below.</p>}
+      body={<p>Interested in hiring me for a <strong>talk</strong>, <strong>webinar</strong>, <strong>workshop</strong>, <strong>podcast</strong>, or <strong>article</strong>? Please contact me through the form below.</p>}
     />
     <Card
       type="main"
