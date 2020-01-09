a607d132f22f7244b7aee76027da7f6c56bde156
Preston So <pso@post.harvard.edu>
2020-01-09 18:11:56 -0500

    Add OCE to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
     <div className="footer__container">
       <h3>Thanks for stopping by.</h3>
       <p>&copy; 2007–{date.getFullYear()} Preston So. <em>Designed entirely in the browser.</em></p>
-      <p>Powered by <a href="https://gatsbyjs.org" target="_blank" rel="noopener noreferrer">Gatsby</a>, <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a>, and <a href="https://www.drupal.org/" target="_blank" rel="noopener noreferrer">Drupal 8</a>.</p>
+      <p>Powered by <a href="https://gatsbyjs.org" target="_blank" rel="noopener noreferrer">Gatsby</a>, <a href="https://www.drupal.org/" target="_blank" rel="noopener noreferrer">Drupal 8</a>, and <a href="https://www.oracle.com/marketingcloud/products/content-and-experience/" target="_blank" rel="noopener noreferrer">Oracle Content and Experience</a>.</p>
       <p>Find me on <a href="https://github.com/prestonso" target="_blank" rel="noopener noreferrer">GitHub</a>, <a href="https://twitter.com/prestonso" target="_blank" rel="noopener noreferrer">Twitter</a>, <a href="https://www.linkedin.com/in/prestonso" target="_blank" rel="noopener noreferrer">LinkedIn</a>, and <a href="https://www.drupal.org/u/prestonso" target="_blank" rel="noopener noreferrer">Drupal.org</a>.</p>
     </div>
   </footer>
