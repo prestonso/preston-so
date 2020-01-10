87456612e0834b9de27cb016d93c1809d9fb4f6a
Preston So <pso@post.harvard.edu>
2020-01-10 17:37:48 -0500

    Move OCE access token to env var

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
       resolve: `gatsby-source-cec`,
       options: {
         contentServer: `https://prestonso-prestonso.cec.ocp.oraclecloud.com/`,
-        channelToken: `25145a1e06a646d0bc58ce0b721aed74`,
+        channelToken: process.env.OCE_ACCESS_TOKEN,
         fromCache: false,
       },
     },
