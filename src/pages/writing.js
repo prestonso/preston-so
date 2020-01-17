4aa3927e8b70a75898258e14d85e56bfe58e84fb
Preston So <pso@post.harvard.edu>
2020-01-17 11:14:59 -0500

    Shorten description

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -13,7 +13,7 @@ const WritingPage = ({ data }) => (
       type="intro"
       orientation="no"
       title="Writing"
-      body={<p>Articles about topics including <strong>headless CMS</strong>, <strong>the JAMstack</strong>, <strong>conversational design</strong>, <strong>decoupled Drupal</strong>, and <strong>the future of content management</strong>.</p>}
+      body={<p>Articles about topics including <strong>headless CMS</strong>, <strong>the JAMstack</strong>, <strong>conversational design</strong>, <strong>decoupled Drupal</strong>, and <strong>the future of content</strong>.</p>}
     />
     <Card
       type="main"
