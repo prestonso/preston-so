488d6161133d8ee8fe5e09a73c961d6fe66ccbfa
Preston So <pso@post.harvard.edu>
2020-03-26 17:36:13 -0400

    Adjust footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby"
 import React from "react"
 
 let date = new Date();
@@ -6,7 +7,7 @@ const Footer = () => (
   <footer>
     <div className="footer__container">
       <h3>Thanks for stopping by.</h3>
-      <p>&copy; 2007–{date.getFullYear()} Preston So. <em>Designed entirely in the browser.</em></p>
+      <p>&copy; 2007–{date.getFullYear()} <Link to="/">Preston So</Link>. <em>Designed entirely in the browser.</em></p>
       <p>Powered by <a href="https://gatsbyjs.org" target="_blank" rel="noopener noreferrer">Gatsby</a>, <a href="https://www.drupal.org/" target="_blank" rel="noopener noreferrer">Drupal 8</a>, and <a href="https://www.oracle.com/marketingcloud/products/content-and-experience/" target="_blank" rel="noopener noreferrer">Oracle Content and Experience</a>.</p>
       <p>Find me on <a href="https://github.com/prestonso" target="_blank" rel="noopener noreferrer">GitHub</a>, <a href="https://twitter.com/prestonso" target="_blank" rel="noopener noreferrer">Twitter</a>, <a href="https://www.linkedin.com/in/prestonso" target="_blank" rel="noopener noreferrer">LinkedIn</a>, and <a href="https://www.drupal.org/u/prestonso" target="_blank" rel="noopener noreferrer">Drupal.org</a>.</p>
     </div>
