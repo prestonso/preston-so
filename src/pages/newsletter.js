60426e435f26711f357830945268bb0c61b1a3f8
Preston So <pso@post.harvard.edu>
2020-04-02 09:07:14 -0400

    Add newsletter page

---
+++ src/pages/newsletter.js
@@ -0,0 +1,46 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import Card from "../components/card"
+
+const NewsletterPage = ({ data }) => (
+  <Layout>
+    <SEO title="Newsletter" keywords={[`newsletter`, `subscribe`, `news`, `monthly`, `updates`]} />
+    <Card
+      type="intro"
+      orientation="no"
+      title="Newsletter"
+      body={<p>No spam. Just musings and exclusive subscriber-only content about headless CMS, voice design, digital experiences, the JAMstack, and more.</p>}
+    />
+    <Card
+      type="main"
+      orientation="no"
+      title="Subscribe"
+      body={
+        <form
+          name="newsletter"
+          method="post"
+          action="/newsletter-success"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="newsletter" />
+          <div className="card__field">
+            <label htmlFor="email">First name</label>
+            <input type="text" name="firstname" id="firstname-newsletter" />
+          </div>
+          <div className="card__field">
+            <label htmlFor="email">E-mail</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <input type="submit" value="Subscribe" className="card__field--special" />
+        </form>
+      }
+    />
+  </Layout>
+)
+
+export default NewsletterPage
