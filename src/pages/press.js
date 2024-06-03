3727737e2f7f437b55ff065170872280e819771c
Preston So <pso@post.harvard.edu>
2024-06-03 16:02:05 -0400

    Temporarily revert from dotCMS to Drupal

--- src/pages/press.js
+++ src/pages/press.js
@@ -22,7 +22,7 @@ const PressPage = ({ data }) => (
       body={
         <div className="card__content__listing">
           <ul>
-            {data.allDotCmsAppearance.edges.map(({ node }) => (
+            {data.allNodeAppearance.edges.map(({ node }) => (
               <li key={node.id}>
                 <h4><a href={node.link} title={node.title}>{node.title}</a></h4>
                 {node.author && <p>{node.author}</p>}
@@ -39,18 +39,23 @@ const PressPage = ({ data }) => (
 
 export const query = graphql`
   query {
-    allDotCmsAppearance(sort: {
-      fields: date
-      order: DESC
-    }) {
+    allNodeAppearance(
+      sort: {
+        fields: [field_date],
+        order: DESC
+      }
+    ) {
       edges {
         node {
           id
           title
-          link: linkUrl
-          author
-          publication
-          date(formatString: "MMMM D, YYYY")
+          link:field_link {
+            uri
+            title
+          }
+          author:field_author
+          publication:field_publication
+          date:field_date
         }
       }
     }
